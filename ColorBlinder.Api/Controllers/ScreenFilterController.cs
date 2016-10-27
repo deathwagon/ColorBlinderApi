6f375c35b13715bb5a52072988d8f8ec6583a8cc
trwalker <trwalker@godaddy.com>
2016-10-27 11:22:15 -0700

    Initial commit

---
+++ ColorBlinder.Api/Controllers/ScreenFilterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing.Imaging;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ColorBlinder.Api.Controllers
+{
+  [Route("v1/ScreenFilter")]
+  public class ScreenFilterController : ApiController
+  {
+    [HttpGet]
+    public JObject GetScreenCaptures(string url)
+    {
+      var driver = new ChromeDriver();
+      driver.Navigate().GoToUrl(url);
+
+      var originalScreenCapture = ((ITakesScreenshot) driver).GetScreenshot();
+      originalScreenCapture.SaveAsFile("C:\\original.jpg", ImageFormat.Jpeg);
+
+      return new JObject(
+        new JProperty("original", "http://localhost/original.jpg")  
+      );
+    }
+  }
+}
